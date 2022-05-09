1e6381554ba62140b6bb44fb186fd51bd486f6e5
Leandro Spanghero Fera <leandrospan@gmail.com>
2022-05-09 19:49:53 -0300

    Tela de Login - Criação

---
+++ src/main/java/com/mycompany/projetojpx/Projetojpx.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.projetojpx;
+
+/**
+ *
+ * @author leandro
+ */
+public class Projetojpx {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+}
